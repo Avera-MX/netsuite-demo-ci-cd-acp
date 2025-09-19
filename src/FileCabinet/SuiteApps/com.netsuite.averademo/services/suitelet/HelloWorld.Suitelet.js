afc7040d1af0339745046bda708c084fb4afb075
MarcosLop <merick16@gmail.com>
2025-09-19 11:11:21 -0600

    feat(ui): enhance greeting field with styled HTML content
    Updated the default value of the greeting field to include a styled HTML block. This change improves the visual presentation by centering text, using a sophisticated color scheme, and adding stylistic elements like shadows and rounded corners. It enhances user engagement by providing an aesthetically pleasing interface.

--- src/FileCabinet/SuiteApps/com.netsuite.averademo/services/suitelet/HelloWorld.Suitelet.js
+++ src/FileCabinet/SuiteApps/com.netsuite.averademo/services/suitelet/HelloWorld.Suitelet.js
@@ -21,7 +21,11 @@ define(['N/ui/serverWidget'],
                 label: 'Saludo',
                 type: serverWidget.FieldType.INLINEHTML
             });
-            greetingField.defaultValue='Holi :D'
+            greetingField.defaultValue=`
+            <div style="text-align: center; font-family: 'Arial', sans-serif; background-color: #f0f8ff; padding: 50px; border-radius: 15px; border: 2px solid #add8e6; color: #333;">
+            <h1 style="color: #4682b4; font-size: 3em; text-shadow: 2px 2px 4px rgba(0,0,0,0.2);">Hello, World!</h1>
+            <p style="font-size: 1.2em; color: #555;">Made with ❤️ by the Avera Engineering Team.</p>
+            </div>`
             scriptContext.response.writePage(form)
         }
 
